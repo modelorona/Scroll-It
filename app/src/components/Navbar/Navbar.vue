e85ca534699f80498402c1ecca65b5157d01c99c
modelorona <angel.hristozov@gmail.com>
2020-05-17 13:45:09 +0100

    added base files to fill in later

---
+++ app/src/components/Navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/Toolbar/Toolbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
