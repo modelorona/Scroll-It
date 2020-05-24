1b5e76c56159322be0d299ef2c0e6a23e103d98a
modelorona <angel.hristozov@gmail.com>
2020-05-24 23:38:25 +0100

    add manifest and pwa stuff

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,5 @@
 const CompressionPlugin = require('compression-webpack-plugin');
+const {GenerateSW} = require('workbox-webpack-plugin');
 
 module.exports = {
     pages: {
@@ -15,6 +16,7 @@ module.exports = {
     ],
     configureWebpack: {
         plugins: [
+            new GenerateSW(),
             new CompressionPlugin()
         ]
     }
